772502b1b9e6bf639a461dee0198fa9de4a4f841
d827 <dpetzol2@binghamton.edu>
2020-02-09 00:49:37 -0500

    fixed after nearly breaking everything

--- main.c
+++ main.c
@@ -13,10 +13,9 @@
 #define ANSI_RED "\x1b[31m"
 
 //Global Variables
-int gameState;
+int gameState = 0;
 
 // Declaration of Functions
-<<<<<<< HEAD
 void setGameState(); //Complete For Now
 int basicMath(); //Completed For Now
 int colorRead(); //Completed For Now
@@ -26,34 +25,24 @@ int readingComp(); //Waiting for GUI
 char profileQ(); //In progress by Dorian
 int typingChallenge(); //Completed for Now
 void firstTime();
-=======
-int setGameState(); //Complete For Now
-int BasicMath(); //Completed For Now
-int ColorRead(); //Completed For Now
-void AudioQ(); //Waiting for GUI
-void ImageQ(); //Waiting for GUI
-void ReadingComp(); //Waiting for GUI
-char ProfileQ(); //In progress by Dorian
-int typing_challenge(); //Completed for Now
->>>>>>> parent of 7bb3b39... debugged main
 
 
 
 
 
-int main(int argc, char **argv){
+int main(){
   int totalScore = 0;
-  int math_score = 0;
-  int type_score = 0;
-  int prof_score = 0;
-  int reading_score = 0;
-  int image_score = 0;
-  int audio_score = 0;
-  
+  int mathScore = 0;
+  int typeScore = 0;
+  int profScore = 0;
+  int readingScore = 0;
+  int imageScore = 0;
+  int audioScore = 0;
+
   int i = 0;
   FILE *filep;
   char persistence[4][1000];
-  filep = fopen("persistence.txt", "r");
+  filep = fopen("persistence2.txt", "r");
   if(filep == NULL){
     gameState = 0;
   }
@@ -63,7 +52,7 @@ int main(int argc, char **argv){
   }
   fclose(filep);
 
-  if(persistence[3] == '1') gameState == 1;
+  if(persistence[3][0] == '1') gameState == 1;
 
   while(gameState == 0){
     firstTime();
@@ -71,15 +60,27 @@ int main(int argc, char **argv){
   while(gameState == 1){
     printf("This is the menu.");
     int choice;
-    printf("Want to start? Press 1 to continue.");
+    printf("Want to start? Press 2 to continue. Press any other key to exit.");
     scanf("%d", choice);
-    if(choice == 1) setGameState(2);
+    if(choice == 2) setGameState(2);
+    else{
+      setGameState(3);
+    }
+
   }
   while(gameState == 2){
-
+    for(int i = 0; i < 3; i++){
+      int result1 = basicMath();
+      if(result1 == 1) mathScore += 1;
+    }
+    totalScore += mathScore;
+    int result2 = typingChallenge();
+    if(result2 == 1) typeScore += 1;
+    totalScore += typeScore;
   }
   while(gameState == 3){
-
+    printf("\nExit.\n");
+    break;
   }
   while(gameState == 4){
 
@@ -92,7 +93,7 @@ void firstTime(){
   initializedGameState[0] = '1';
   initializedGameState[1] = '\0';
   printf("Hello and welcome to Detox.me! Please complete this quick first time setup.\n");
-  printf("Please enter your name:\n");
+  printf("Please enter your first name:\n");
   scanf("%s", &name);
   printf("\nPlease enter your birthdate (formatted MMDDYYYY):\n");
   scanf("%s", &bday);
@@ -101,8 +102,8 @@ void firstTime(){
   printf("\n");
 
   FILE *fp;
-  fp = fopen("persistence.txt", "w");
-  if(fp == NULL) printf("persistence.txt has failed to open.");
+  fp = fopen("persistence2.txt", "w");
+  if(fp == NULL) printf("persistence2.txt has failed to open.");
   else{
     fputs(name, fp);
     fputs("\n", fp);
@@ -118,9 +119,9 @@ void firstTime(){
   }
 }
 
-void play(){
-
-}
+// void play(){
+//
+// }
 
 void setGameState(int n){
   if(n == 0) gameState = 0; //First time startup
@@ -130,24 +131,24 @@ void setGameState(int n){
   if(n == 4) gameState = 4; //Error screen
 }
 
-int BasicMath(int v1, int v2, int eq, int M_Score){
+int basicMath(){
+  int score;
   srand(time(0));
-  v1 = rand() % 100; //First Variable
-  v2 = rand() % 100; //Second Variable
-  eq = v1 + v2; //Eq value
-  print("What is the value of %d + %d? :> ",v1,v2);//Print Statement asking for values
+  int v1 = rand() % 100; //First Variable
+  int v2 = rand() % 100; //Second Variable
+  int eq = v1 + v2; //Eq value
+  printf("What is the value of %d + %d? :> ",v1,v2);//Print Statement asking for values
   scanf("%d",&eq);   //Equation input
   if(eq==v1+v2){
     score = 1;
-    return score;
   }
   else{
     score = 0;
-    return M_Score;
-  }                //Value Checking
+  }
+  return score;
 }
 
-int typing_challenge(){
+int typingChallenge(){
 
     srand(time(NULL));
 
@@ -186,11 +187,11 @@ int typing_challenge(){
 
       if (result == 0){
         printf("\nCorrect!\n");
-        score = score + 1;}
-
+        score = score + 1;
+      }
       else  {
         printf("\nWrong!\n");
-        fail = fail + 1;}
+        fail = fail + 1;
 
       }
     fclose (infile);
@@ -205,68 +206,98 @@ int typing_challenge(){
       printf("\nYou Failed!");
       return 0;
     }
-
   }
-
-
 }
 
-int ColorRead(){
-//Initializing Values 0-3 for array values
-  int score;
-  srand(time(0));
-  char colors[4] = {CRED,CGRN,CYEL,CBLU}; //Array storing labels of different color strings
-  char colorWords[4] = {"Red","Green","Yellow","Blue"}
-  int color = rand() % 4; //random values chosen for array
-  int colorWord = rand() % 4; //Value for ColorWord Array
-  // int color2 = rand() % 4;
-  // int color3 = rand() % 4;
-  // int color4 = rand() % 4;
-//Declaring color variables
-  colors[0] = CRED;
-  colors[1] = CGRN;
-  colors[2] = CBLU;
-  colors[3] = CYEL;
-
-/* Print and Scan statements for retrieving Inputs */
-
-//First Test
-  char ans[6];
-  printf("What does this word say -> %s", colors[color]);
-  printf("%s ?", colorWords[colorWord]);
-  printf(colors[2]);
-
-  scanf("%s", &ans);
-  if(strcmp(ans, colorWords[colorWord]) == 0){
-    score += 1;
-  }
-  else{
-    score += 0;
-  }
-
-//Second Test
-//   char Second;
-//   printf("What is the color on this word -> %sGreen?\n",colors[color2]);
-//   scanf("%s",&Second);
-//   if(Second == colors[color2]){
+// int colorRead(){
+// //Initializing Values 0-3 for array values
+//   int score;
+//   srand(time(0));
+//   char colors[4] = {CRED,CGRN,CYEL,CBLU}; //Array storing labels of different color strings
+//   char colorWords[4] = {"Red","Green","Yellow","Blue"}
+//   int color = rand() % 4; //random values chosen for array
+//   int colorWord = rand() % 4; //Value for ColorWord Array
+//   // int color2 = rand() % 4;
+//   // int color3 = rand() % 4;
+//   // int color4 = rand() % 4;
+// //Declaring color variables
+//   colors[0] = CRED;
+//   colors[1] = CGRN;
+//   colors[2] = CBLU;
+//   colors[3] = CYEL;
+//
+// /* Print and Scan statements for retrieving Inputs */
+//
+// //First Test
+//   char ans[6];
+//   printf("What does this word say -> %s", colors[color]);
+//   printf("%s ?", colorWords[colorWord]);
+//   printf(colors[2]);
+//
+//   scanf("%s", &ans);
+//   if(strcmp(ans, colorWords[colorWord]) == 0){
 //     score += 1;
 //   }
 //   else{
 //     score += 0;
 //   }
 //
-// //Third Test
-//   char Third;
-//   printf("What is the color on this word -> %sBlue?\n",colors[color3]);
-//   scanf("%s",&Third);
-//   if(Third == colors[color3]){
-//     score += 1;
-//   }
-//   else{
-//     score += 0;
-//   }
+// //Second Test
+// //   char Second;
+// //   printf("What is the color on this word -> %sGreen?\n",colors[color2]);
+// //   scanf("%s",&Second);
+// //   if(Second == colors[color2]){
+// //     score += 1;
+// //   }
+// //   else{
+// //     score += 0;
+// //   }
+// //
+// // //Third Test
+// //   char Third;
+// //   printf("What is the color on this word -> %sBlue?\n",colors[color3]);
+// //   scanf("%s",&Third);
+// //   if(Third == colors[color3]){
+// //     score += 1;
+// //   }
+// //   else{
+// //     score += 0;
+// //   }
+// //
+// // //Fourth Test
+// //   char Fourth;
+// //   printf("What is the color on this word -> %sYellow?\n",)colors[color4];
+// //   scanf("%s",&Fourth);
+// //   if(Fourth == color[color4]){
+// //     score += 1;
+// //   }
+// //   else{
+// //     score += 0;
+// //   }
+// //   return score;
+// }
 //
-<<<<<<< HEAD
+// /*typedef struct Color{
+//   uint8_t R;
+//   uint8_t G;
+//   uint8_t B;
+// }colortype;
+//
+// colortype red;
+// red.R = 255U;
+// red.G = 0U;
+// red.B = 0U;
+//
+// colortype green;
+// green.R = 0U;
+// green.G = 255U;
+// green.B = 0U;
+//
+// colortype blue;
+// blue.R = 0U;
+// blue.G = 0U;
+// blue.B = 255U;
+//
 // colortype yellow;
 // yellow.R = 244U;
 // yellow.G = 232U;
@@ -337,44 +368,3 @@ int readingComp() {
 
   return 0;
 }
-=======
-// //Fourth Test
-//   char Fourth;
-//   printf("What is the color on this word -> %sYellow?\n",)colors[color4];
-//   scanf("%s",&Fourth);
-//   if(Fourth == color[color4]){
-//     score += 1;
-//   }
-//   else{
-//     score += 0;
-//   }
-//   return score;
-}
-
-/*typedef struct Color{
-  uint8_t R;
-  uint8_t G;
-  uint8_t B;
-}colortype;
-
-colortype red;
-red.R = 255U;
-red.G = 0U;
-red.B = 0U;
-
-colortype green;
-green.R = 0U;
-green.G = 255U;
-green.B = 0U;
-
-colortype blue;
-blue.R = 0U;
-blue.G = 0U;
-blue.B = 255U;
-
-colortype yellow;
-yellow.R = 244U;
-yellow.G = 232U;
-yellow.B = 104U;
-*/
->>>>>>> parent of 7bb3b39... debugged main
